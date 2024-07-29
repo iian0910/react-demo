cb9d143044549bbbb5f59329bb2f3ef8415ae880
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2024-07-29 15:24:41 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// 外部套件
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// 內部套件
+import logo from './assets/images/logo.svg';
+import './assets/style/App.css';
+import Input from './components/Input';
+import './assets/style/all.scss'
+
+
 
 function App() {
+  const [text, setText] = useState('')
+  const onChangeHandler = (e) => {
+    setText(e.target.value)
+  }
+
+  useEffect(() => {
+    (async() => {
+      const { data } = await axios.get(process.env.REACT_APP_PATH_URL)
+      console.log('result =>', data)
+    })()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Quis ut sint est deserunt mollit minim cupidatat ea consequat aliqua do commodo aliquip.
         </p>
+        <button type='button' className='btn btn-primary'>Primary</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +39,14 @@ function App() {
         >
           Learn React
         </a>
+        <br/>
+        <Input
+          id="sampleText"
+          text="這是一個 Input"
+          value={text}
+          onChangeHandler={onChangeHandler}
+        />
+        { text }
       </header>
     </div>
   );
---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+const Input = ({id, text, value, onChangeHandler}) => {
+  return (
+    <>
+      <label htmlFor={id} className="">{ text }</label>
+      <input
+        type="text"
+        id={id}
+        value={value}
+        onChange={onChangeHandler}
+      ></input>
+    </>
+  )
+}
+
+export default Input
